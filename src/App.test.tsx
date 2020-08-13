c1f1ea63189505b5ffcaa411092608bfea968854
Iain van der Wiel <iain.vanderwiel@gmail.com>
2020-08-13 09:06:32 +0200

    Add user-event and failing test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("click", async () => {
+  const { getByRole } = render(<textarea />);
+
+  await userEvent.type(getByRole("textbox"), "Hello, World!");
+  expect(getByRole("textbox")).toHaveAttribute("value", "Hello, World!");
 });
